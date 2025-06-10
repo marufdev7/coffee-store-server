a3a5d4014c92964af73249e02427db5b8a8e8cc6
mrfhmd7 <mrf.hmd7@gmail.com>
2025-06-10 11:56:02 +0600

    get a single coffee and delete a coffee

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -35,10 +35,24 @@ async function run() {
             res.send(result);
         });
 
+        app.get('/coffee/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await coffeeCollection.findOne(query);
+            res.send(result);
+        })
+
         app.post('/coffee', async (req, res) => {
             const coffee = req.body;
             console.log(coffee);
             const result = await coffeeCollection.insertOne(coffee);
+            res.send(result);
+        });
+
+        app.delete('/coffee/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await coffeeCollection.deleteOne(query);
             res.send(result);
         });
 
